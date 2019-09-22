90ab693d2771bed2c1e05645c406c3f692661494
Mason Trimbell <mtmaster3@gmail.com>
2019-09-22 12:19:00 -0700

    Setup mongoDB & user model

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup')
+const mongoose = require('mongoose');
+const keys = require('./config/keys')
 
 const app = express();
 
 // set up view engine
 app.set('view engine', 'ejs');
 
+mongoose.connect(keys.mongodb.dbURI, () => {
+    console.log('connected to mongodb');
+});
+
 // set up routes
-app.use('/auth', authRoutes)
+app.use('/auth', authRoutes);
 
 // create home route
 app.get('/', (req, res) => {
