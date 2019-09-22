83b0f39fc92b7c3d6b45a0f135a94e617930cb8c
Mason Trimbell <mtmaster3@gmail.com>
2019-09-22 00:39:12 -0700

    Setup passport callback function

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -12,14 +12,13 @@ router.get('/logout', (req, res) => {
     res.send('logging out');
 });
 
-
 // auth with google
 router.get('/google', passport.authenticate('google', {
     scope: ['profile']
 }));
 
 // callback route for google to redirect to
-router.get('/google/redirect', (req, res) => {
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
     res.send('you reached the callback URI')
 });
 
