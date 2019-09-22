f041fd3233d824750cb5d32569ab9a5dbdcd9ca2
Mason Trimbell <mtmaster3@gmail.com>
2019-09-22 14:11:47 -0700

    Update user model and passport setup

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../models/user-model')
 
 passport.use(
     new GoogleStrategy({
@@ -9,6 +10,27 @@ passport.use(
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret
     }, (accessToken, refreshToken, profile, done) => {
-        // passport callback function
+        // check if user already exists in DB
+        User.findOne({googleId: profile.id}).then((currentUser) => {
+            if (currentUser) {
+                // user already exists
+                console.log('uesr is: ', currentUser);
+            }
+            else if (profile._json.hd == 'edmonds15.org') {
+                // if not, create new user
+                console.log(profile);
+                new User({
+                    fullName: profile.displayName,
+                    firstName: profile.name.givenName,
+                    googleId: profile.id,
+                    email: profile.emails[0].value
+                }).save().then((newUser) => {
+                    console.log('new user created' + newUser);
+                });        
+            }
+            else {
+                console.log('not in ESD domain');
+            }
+        });
     })
 );
